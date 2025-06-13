3d9a2f4e14ba544e100f510e0969eaaead2d3520
rajifmahendra <rajifmahendra10@gmail.com>
2025-06-13 17:17:03 +0700

    update vite

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        host: true, // Izinkan akses dari jaringan lain
-        port: 5173, // Pastikan ini sesuai dengan yang digunakan
-        strictPort: true,
-        hmr: {
-            clientPort: 443, // Untuk ngrok agar bisa bekerja di HTTPS
-        },
-        allowedHosts: [
-            '.ngrok.io',  // Jika menggunakan subdomain ngrok lama
-            '.ngrok-free.app'  // Jika menggunakan domain ngrok baru
-        ],
-    },
+  plugins: [react()],
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: 'localhost',
+  },
 });
